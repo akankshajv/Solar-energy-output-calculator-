b9ab2a23f1f3557c88d314472ed1c8d755540fa8
Ashish Mallar <ashishmallar@Ashishs-MacBook-Air.local>
2026-03-01 14:56:04 +0530

    Reverted to commit 694958a as discussed

--- Model Training & Evaluation/Training_Model.py
+++ Model Training & Evaluation/Training_Model.py
@@ -2,47 +2,65 @@ import pandas as pd
 import numpy as np
 import joblib
 
+from sklearn.preprocessing import MinMaxScaler
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import mean_squared_error, r2_score, mean_absolute_error
 
-data = pd.read_csv("cleaned_synthetic_solar_dataset.csv")
+import os
 
-features = [ "cloud_cover","direct_sunlight_hours", "solar_radiation","panel_efficiency"]
-X = data[features]
-y = data["generated_power_kw"]
 
-from sklearn.model_selection import train_test_split
+# Get project root directory
+current_file = os.path.abspath(__file__)
+model_folder = os.path.dirname(current_file)
+project_root = os.path.dirname(model_folder)
 
-X_train, X_test, y_train, y_test = train_test_split(
-X, y, test_size=0.2, random_state=42
-)
+train_path = os.path.join(project_root, "preprocessing", "train_dataset.csv")
+test_path = os.path.join(project_root, "preprocessing", "test_dataset.csv")
+
+train_data = pd.read_csv(train_path)
+test_data = pd.read_csv(test_path)
+
+
+features = ["cloud_cover","direct_sunlight_hours","solar_radiation","panel_efficiency"]
+X_train = train_data[features]
+
+y_train = train_data["generated_power_kw"]
+
+X_test = test_data[["cloud_cover","direct_sunlight_hours","solar_radiation","panel_efficiency"]]
+y_test = test_data["generated_power_kw"]
+
+
+
+scaler = MinMaxScaler()
+X_train_scaled = scaler.fit_transform(X_train)
+X_test_scaled = scaler.transform(X_test)
+
 model = RandomForestRegressor(
     n_estimators=500,
-    max_depth=15,
-    min_samples_split=5,
-    min_samples_leaf=2,
+    max_depth=25,
+    min_samples_split=2,
+    min_samples_leaf=1,
     random_state=42,
     n_jobs=-1
 )
+model.fit(X_train_scaled, y_train)
 
-model.fit(X_train, y_train)
-y_pred = model.predict(X_test)
+
+y_pred = model.predict(X_test_scaled)
 
 mse = mean_squared_error(y_test, y_pred)
+
 rmse = np.sqrt(mse)
-mae = mean_absolute_error(y_test, y_pred)
-r2 = r2_score(y_test, y_pred)
+mae = mean_absolute_error(y_test, (y_pred))
+r2 = r2_score(y_test, (y_pred))
 
 print("Mean Absolute Error:", mae)
 print("Mean Squared Error:", mse)
 print("RMSE:", rmse)
 print("R^2 Score:", r2)
+model_path = os.path.join(project_root, "model.pkl")
+scaler_path = os.path.join(project_root, "scaler.pkl")
+joblib.dump(model, model_path)
+joblib.dump(scaler, scaler_path)
 
-mean_power = y.mean()
-error_percent = (rmse / mean_power) * 100
-
-print("Average Power:", mean_power)
-print("RMSE as % of Average Power:", error_percent, "%")
-
-joblib.dump(model, "model.pkl")
-print("Model saved successfully!")
+print("Model and Scaler saved successfully!")
